2e0daecf33533d29141f2eb8d990038d16e2a73a
Devam Agrawal <dev.devam.a@gmail.com>
2022-07-14 07:41:06 +0530

    Submission Files

---
+++ JS/external.js
@@ -0,0 +1,13 @@
+function alertexternal(){
+    alert("You are in external javascript");
+}
+
+function confirmexternal(){
+confirm("Are you sure..??")
+}
+
+function promptexternal(){
+    let first=prompt('Enter your first name');
+    let second=prompt('Enter your second name');
+    alert("Your name is '"+first+" "+second+"'");
+}
